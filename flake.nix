b93c34a8a73be0e2b38c8025609f988021b3e212
Dan Nixon <dan@dan-nixon.com>
2026-06-24 17:30:15 +0100

    Add demo firmware (#1)

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        # Toolchain
+        rustup
+        espup
+
+        # Upload/debug tool
+        probe-rs-tools
+
+        # Code formatting tools
+        treefmt
+        alejandra
+        mdl
+        rustfmt
+        typos
+      ];
+
+      DBX_CONTAINER_IMAGE = "ubuntu:26.04";
+      DBX_CONTAINER_NAME = "ethernet-hexpansion-build-environment";
+    };
+  };
+}
